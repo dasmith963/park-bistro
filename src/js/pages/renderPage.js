770af7e86988172fa797e3d7c3f913ca28b5da8d
Devon <Devon.smith017@gmail.com>
2023-10-26 22:42:52 -0500

    Add function to create the nav element

--- src/js/pages/renderPage.js
+++ src/js/pages/renderPage.js
@@ -1,15 +1,35 @@
-export default function renderPage(){
+export default function renderPage() {
   const siteContainer = document.getElementById('site-container');
-
   siteContainer.appendChild(createHeader('Park Bistro'))
 }
 
 
-function createHeader(restaurantName){
+function createHeader(restaurantName) {
   const header = document.createElement('header');
   const logo = document.createElement('h1');
-  logo.textContent = restaurantName;
+  const nav = createNav(['Home', 'Menu', 'Contact']);
 
-  header.appendChild(logo);
+  logo.textContent = restaurantName;
+  header.append(logo, nav);
+
   return header;
+}
+
+function createNav(links) {
+  const nav = document.createElement('nav');
+  const ul = document.createElement('ul');
+
+  links.forEach(link => {
+    const li = document.createElement('li');
+
+    const anchor = document.createElement('a');
+    anchor.textContent = link;
+    anchor.setAttribute('href', '#' + link);
+
+    li.appendChild(anchor);
+    ul.appendChild(li);
+  })
+
+  nav.appendChild(ul);
+  return nav;
 }
