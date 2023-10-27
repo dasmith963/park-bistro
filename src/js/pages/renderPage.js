e65b57717c3d979a4e1be0776a6360a7a918530c
Devon <Devon.smith017@gmail.com>
2023-10-26 22:32:40 -0500

    Add function to render page elements

--- src/js/pages/renderPage.js
+++ src/js/pages/renderPage.js
@@ -1,8 +1,15 @@
+export default function renderPage(){
+  const siteContainer = document.getElementById('site-container');
+
+  siteContainer.appendChild(createHeader('Park Bistro'))
+}
+
+
 function createHeader(restaurantName){
   const header = document.createElement('header');
   const logo = document.createElement('h1');
   logo.textContent = restaurantName;
-  
+
   header.appendChild(logo);
   return header;
 }
