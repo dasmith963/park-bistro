fe4c7f45368c686a580ae2c058f8c30199cb541c
Devon <Devon.smith017@gmail.com>
2023-10-27 22:47:29 -0500

    Rework create hero section function

--- src/js/pages/home.js
+++ src/js/pages/home.js
@@ -1,3 +1,5 @@
+import addText from "../helpers/addText";
+
 export default function loadHomePage() {
   const main = document.querySelector('.content');
   main.replaceChildren(createHomeSection());
@@ -11,18 +13,14 @@ function createHomeSection() {
   return homeSection;
 }
 
-function createHeroSection(){
-  const heroSection = document.createElement('div');
+function createHeroSection() {
+  const heroContainer = document.createElement('div');
 
-  const slogan = document.createElement('p');
-  slogan.textContent = 'Indulge in Nature\'s Bounty'
+  heroContainer.append(
+    addText('Indulge in Nature\'s Bounty'),
+    addText('Meals crafted with heart, served with style.'),
+    addText('We invite you to savor every moment, every flavor, and every memory.')
+  );
 
-  const welcomeText = document.createElement('p');
-  welcomeText.textContent = 'Meals crafted with heart, served with style.'
-
-  const callToAction = document.createElement('p');
-  callToAction.textContent = 'We invite you to savor every moment, every flavor, and every memory.'
-
-  heroSection.append(slogan, welcomeText, callToAction);
-  return heroSection;
+  return heroContainer;
 }
