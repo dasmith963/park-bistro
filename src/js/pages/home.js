fc096f059a318297e0bacf8002ca0027a8258047
Devon <Devon.smith017@gmail.com>
2023-10-27 22:34:39 -0500

    Add function to create the hero section

--- src/js/pages/home.js
+++ src/js/pages/home.js
@@ -6,5 +6,23 @@ export default function loadHomePage() {
 function createHomeSection() {
   const homeSection = document.createElement('section');
   homeSection.classList.add('home');
+
+  homeSection.appendChild(createHeroSection());
   return homeSection;
+}
+
+function createHeroSection(){
+  const heroSection = document.createElement('div');
+
+  const slogan = document.createElement('p');
+  slogan.textContent = 'Indulge in Nature\'s Bounty'
+
+  const welcomeText = document.createElement('p');
+  welcomeText.textContent = 'Meals crafted with heart, served with style.'
+
+  const callToAction = document.createElement('p');
+  callToAction.textContent = 'We invite you to savor every moment, every flavor, and every memory.'
+
+  heroSection.append(slogan, welcomeText, callToAction);
+  return heroSection;
 }
