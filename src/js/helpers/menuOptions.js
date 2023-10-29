93c36a9380d2b9c1e437e368ad804b52554f3c0a
Devon <Devon.smith017@gmail.com>
2023-10-29 12:09:48 -0500

    Add function to return the appetizers array

--- src/js/helpers/menuOptions.js
+++ src/js/helpers/menuOptions.js
@@ -5,3 +5,25 @@ class Meal {
     this.description = description;
   }
 }
+
+export function getAppetizers() {
+  const appetizer1 = new Meal(
+    'Crispy Calamari',
+    '$9.99',
+    'Tender calamari rings served with marinara sauce.'
+  )
+
+  const appetizer2 = new Meal(
+    'Stuffed Mushrooms',
+    '$9.99',
+    'Mushrooms filled with herbed cream cheese and baked to perfection.'
+  )
+
+  const appetizer3 = new Meal(
+    'Garlic Parmesan Fries',
+    '$9.99',
+    'Crispy shoestring fries tossed in garlic and Parmesan cheese.'
+  )
+
+  return [appetizer1, appetizer2, appetizer3];
+}
