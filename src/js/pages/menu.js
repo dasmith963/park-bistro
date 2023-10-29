9b600c44f0c53a28cf5ea795b15e783ce06d8c0d
Devon <Devon.smith017@gmail.com>
2023-10-28 19:38:26 -0500

    Add function to create menu section

--- src/js/pages/menu.js
+++ src/js/pages/menu.js
@@ -1,4 +1,10 @@
 export default function loadMenuPage() {
   const main = document.querySelector('.content');
-  main.replaceChildren();
+  main.replaceChildren(createMenuSection());
+}
+
+function createMenuSection(){
+  const menuSection = document.createElement('section');
+  menuSection.classList.add('menu');
+  return menuSection;
 }
