52cd8ec5b8274584167d356cf218107a800080c2
Devon <Devon.smith017@gmail.com>
2023-10-28 19:30:28 -0500

    Chore: Add semicolon to the last line of function

--- src/js/helpers/addText.js
+++ src/js/helpers/addText.js
@@ -1,5 +1,5 @@
 export default function addText(text) {
-  const para = document.createElement('p')
+  const para = document.createElement('p');
   para.textContent = text;
-  return para
+  return para;
 }
