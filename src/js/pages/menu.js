5589b4f81ea2b4264c8d1152cd04979da0115237
Devon <Devon.smith017@gmail.com>
2023-10-28 22:41:15 -0500

    Add function to create meal item divs

--- src/js/pages/menu.js
+++ src/js/pages/menu.js
@@ -1,10 +1,29 @@
+import addText from "../helpers/addText";
+
 export default function loadMenuPage() {
   const main = document.querySelector('.content');
   main.replaceChildren(createMenuSection());
 }
 
-function createMenuSection(){
+function createMenuSection() {
   const menuSection = document.createElement('section');
   menuSection.classList.add('menu');
   return menuSection;
+}
+
+function createMealItem(category, meals) {
+  const mealContainer = document.createElement('div');
+  const mealItem = document.createElement('div');
+  const mealCategory = document.createElement('h3');
+  mealCategory.textContent = category;
+
+  meals.forEach(meal => {
+    const mealTitle = addText(`${meal.title}`);
+    const mealPrice = addText(`${meal.price}`);
+    const mealDescription = addText(`${meal.description}`);
+    mealItem.append(mealTitle, mealPrice, mealDescription);
+  })
+
+  mealContainer.append(mealCategory, mealItem);
+  return mealContainer;
 }
