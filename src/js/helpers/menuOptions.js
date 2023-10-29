2d576d57c79c45f372e50841f7f394ded13f5c62
Devon <Devon.smith017@gmail.com>
2023-10-29 12:51:36 -0500

    Add function to return the main courses array

--- src/js/helpers/menuOptions.js
+++ src/js/helpers/menuOptions.js
@@ -27,3 +27,25 @@ export function getAppetizers() {
 
   return [appetizer1, appetizer2, appetizer3];
 }
+
+export function getMainCourses() {
+  const mainCourse1 = new Meal(
+    'Grilled Snapper',
+    '9.99',
+    'Fresh Gulf snapper grilled and drizzled with a lemon beurre blanc sauce, accompanied by jambalaya.'
+  )
+
+  const mainCourse2 = new Meal(
+    'Penne Primavera',
+    '9.99',
+    'Penne pasta with a medley of sautéed vegetables in a garlic and olive oil sauce.'
+  )
+
+  const mainCourse3 = new Meal(
+    'Lemon Herb Roasted Chicken',
+    '9.99',
+    'Half chicken roasted with lemon, garlic, and fresh herbs, accompanied by roasted vegetables.'
+  )
+
+  return [mainCourse1, mainCourse2, mainCourse3];
+}
