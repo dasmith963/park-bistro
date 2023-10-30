504ec0da29c6710bd81417749b3b2ec3c32650e9
Devon <Devon.smith017@gmail.com>
2023-10-29 21:18:28 -0500

    Import function to load the contact page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import renderPage from "./js/pages/renderPage";
 import loadHomePage from "./js/pages/home";
 import loadMenuPage from "./js/pages/menu";
+import loadContactPage from "./js/pages/contact";
 
 renderPage();
 loadHomePage();
 loadMenuPage();
+loadContactPage();
