1305d8fba7d0f0f7032a74602f7f44bf837528e1
Devon <Devon.smith017@gmail.com>
2023-10-29 21:55:24 -0500

    Add function to create contact infomation div

--- src/js/pages/contact.js
+++ src/js/pages/contact.js
@@ -1,3 +1,5 @@
+import addText from "../helpers/addText";
+
 export default function loadContactPage() {
   const main = document.querySelector('.content');
   main.replaceChildren(createContactSection());
@@ -6,5 +8,23 @@ export default function loadContactPage() {
 function createContactSection() {
   const contactSection = document.createElement('section');
   contactSection.classList.add('contact');
+
+  contactSection.append(createContactInfo());
+
   return contactSection;
+}
+
+function createContactInfo() {
+  const container = document.createElement('div');
+
+  const sectionHeader = document.createElement('h2');
+  sectionHeader.textContent = 'We\'d love to hear from you';
+
+  container.append(
+    sectionHeader,
+    addText('Please call us @ (555) 555-5555'),
+    addText('or email us using the contact form')
+  );
+
+  return container;
 }
