852d2b8f30806d985c89db376c5c7f128450fda7
Devon <Devon.smith017@gmail.com>
2023-10-29 21:22:10 -0500

    Add function to create the contact page section

--- src/js/pages/contact.js
+++ src/js/pages/contact.js
@@ -1,4 +1,10 @@
 export default function loadContactPage() {
   const main = document.querySelector('.content');
-  main.replaceChildren();
+  main.replaceChildren(createContactSection());
+}
+
+function createContactSection() {
+  const contactSection = document.createElement('section');
+  contactSection.classList.add('contact');
+  return contactSection;
 }
