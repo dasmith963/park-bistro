c0d2ce6249fbbc6b52fab555f6840c5be2fd6a65
Devon <Devon.smith017@gmail.com>
2023-10-29 20:28:23 -0500

    Import load menu page function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import renderPage from "./js/pages/renderPage";
 import loadHomePage from "./js/pages/home";
+import loadMenuPage from "./js/pages/menu";
 
 renderPage();
 loadHomePage();
+loadMenuPage();
