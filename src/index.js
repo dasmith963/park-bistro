a2ca2ae26b40597f75467c82c23bc973e0d7e270
Devon <Devon.smith017@gmail.com>
2023-10-31 22:58:11 -0500

    Add function to update main content section

--- src/index.js
+++ src/index.js
@@ -3,7 +3,23 @@ import loadHomePage from "./js/pages/home";
 import loadMenuPage from "./js/pages/menu";
 import loadContactPage from "./js/pages/contact";
 
+document.addEventListener('click', updateMainContent)
+
+function updateMainContent(e) {
+  const currentTab = e.target.closest('[data-id]');
+
+  if (!currentTab) return;
+
+  if (currentTab.dataset.id === 'home') {
+    loadHomePage();
+  }
+  if (currentTab.dataset.id === 'menu') {
+    loadMenuPage();
+  }
+  if (currentTab.dataset.id === 'contact') {
+    loadContactPage();
+  }
+}
+
 renderPage();
 loadHomePage();
-loadMenuPage();
-loadContactPage();
