05dd2d9eb76f79a83cb8833e29f442c7132518e7
Devon <Devon.smith017@gmail.com>
2023-11-05 20:46:42 -0600

    Fix: meal item price

--- src/js/helpers/menuOptions.js
+++ src/js/helpers/menuOptions.js
@@ -9,19 +9,19 @@ class Meal {
 export function getAppetizers() {
   const appetizer1 = new Meal(
     'Crispy Calamari',
-    '$9.99',
+    '$9',
     'Tender calamari rings served with marinara sauce.'
   )
 
   const appetizer2 = new Meal(
     'Stuffed Mushrooms',
-    '$9.99',
+    '$9',
     'Mushrooms filled with herbed cream cheese and baked to perfection.'
   )
 
   const appetizer3 = new Meal(
     'Garlic Parmesan Fries',
-    '$9.99',
+    '$9',
     'Crispy shoestring fries tossed in garlic and Parmesan cheese.'
   )
 
@@ -31,19 +31,19 @@ export function getAppetizers() {
 export function getMainCourses() {
   const mainCourse1 = new Meal(
     'Grilled Snapper',
-    '9.99',
+    '$9',
     'Fresh Gulf snapper grilled and drizzled with a lemon beurre blanc sauce.'
   )
 
   const mainCourse2 = new Meal(
     'Penne Primavera',
-    '9.99',
+    '$9',
     'Penne pasta with a medley of sautéed vegetables in a garlic and olive oil sauce.'
   )
 
   const mainCourse3 = new Meal(
-    'Lemon Herb Roasted Chicken',
-    '9.99',
+    'Roasted Chicken',
+    '$9',
     'Half chicken roasted with lemon, garlic, and fresh herbs, served with roasted vegetables.'
   )
 
@@ -53,19 +53,19 @@ export function getMainCourses() {
 export function getDesserts() {
   const dessert1 = new Meal(
     'Tiramisu',
-    '9.99',
+    '$9',
     'Classic Italian dessert made with layers of coffee-soaked ladyfingers and mascarpone cheese.'
   )
 
   const dessert2 = new Meal(
     'Chocolate Lava Cake',
-    '9.99',
+    '$9',
     'Warm, gooey chocolate cake with a molten center, topped with vanilla ice cream.'
   )
 
   const dessert3 = new Meal(
     'The Parc Cheesecake',
-    '9.99',
+    '$9',
     'Creamy cheesecake with a graham cracker crust, served with berry compote.'
   )
 
@@ -75,19 +75,19 @@ export function getDesserts() {
 export function getDrinks() {
   const drink1 = new Meal(
     'Ambrosia Bliss',
-    '9.99',
+    '$9',
     'A champagne cocktail with gin, champagne, lemon juice, and sugar.'
   )
 
   const drink2 = new Meal(
     'Sazerac',
-    '9.99',
+    '$9',
     'A classic cocktail featuring rye whiskey, absinthe, and bitters.'
   )
 
   const drink3 = new Meal(
     'Mint Julep Elegance',
-    '9.99',
+    '$9',
     'A refined mint julep made with top-shelf bourbon and fresh mint.'
   )
 
